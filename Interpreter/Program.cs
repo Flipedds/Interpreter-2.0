e1d77e4f73c7687fc2856e66229bb63322cf7a01
Filipe André <flipeandre12@gmail.com>
2024-01-06 13:21:08 -0300

    refactor: trocando if's por switch pattern

--- Interpreter/Program.cs
+++ Interpreter/Program.cs
@@ -10,51 +10,53 @@ Patterns patterns = new();
 
 while (!sr.EndOfStream)
 {
-    if (line != null
-    && Regex.IsMatch(line, patterns.Print)
-    | Regex.IsMatch(line, patterns.PrintNumber)
-    | Regex.IsMatch(line, patterns.PrintVar))
+    switch (line)
     {
-        ValidationService.PrintValidation(
-            patterns, ref line, ref repo.LineCount, 
-            ref repo.FuncList, ref repo.VarList, repo.NameFunc, ref sr,
-            repo.LineFunc);
-    }
-    else if (line != null
-    && Regex.IsMatch(line, patterns.Def))
-    {
-        ValidationService.DefValidation(
-            ref line, ref repo.FuncList, ref repo.NameFunc, 
+        case string s when s != null &&
+                         (Regex.IsMatch(s, patterns.Print) ||
+                         Regex.IsMatch(s, patterns.PrintNumber) ||
+                         Regex.IsMatch(s, patterns.PrintVar)):
+            ValidationService.PrintValidation(
+                patterns, ref line, ref repo.LineCount,
+                ref repo.FuncList, ref repo.VarList, repo.NameFunc, ref sr,
+                repo.LineFunc);
+            break;
+
+        case string s when s != null &&
+                        Regex.IsMatch(s, patterns.Def):
+            ValidationService.DefValidation(
+            ref line, ref repo.FuncList, ref repo.NameFunc,
             ref repo.LineFunc, ref repo.LineCount, ref sr);
-    }
-    else if (line != null
-    && Regex.IsMatch(line, patterns.ExecDef))
-    {
-        ValidationService.ExecDefValidation(
-            patterns, ref line, ref repo.FuncList, 
-            ref repo.LineCount, repo.NameFunc, ref sr, 
+            break;
+
+        case string s when s != null &&
+                        Regex.IsMatch(s, patterns.ExecDef):
+            ValidationService.ExecDefValidation(
+            patterns, ref line, ref repo.FuncList,
+            ref repo.LineCount, repo.NameFunc, ref sr,
             ref repo.VarList, repo.LineFunc);
-    }
-    else if (line == "" | string.IsNullOrWhiteSpace(line))
-    {
-        ValidationService.IsNullOrWhiteSpaceValidation(
-            ref line, ref repo.NameFunc, 
+            break;
+
+        case string s when s == "" ||
+                        string.IsNullOrWhiteSpace(s):
+            ValidationService.IsNullOrWhiteSpaceValidation(
+            ref line, ref repo.NameFunc,
             ref repo.LineFunc, ref repo.LineCount, ref sr);
-    }
-    else if (line != null
-    && line.Contains('='))
-    {
-        ValidationService.VarValidation(
-            repo.NameFunc, patterns, ref line, 
+            break;
+
+        case string s when s != null && 
+                        line.Contains('='):
+            ValidationService.VarValidation(
+            repo.NameFunc, patterns, ref line,
             ref repo.FuncList, ref repo.LineCount, ref repo.VarList, ref sr,
             repo.LineFunc);
-    }
-    else
-    {
-        Console.WriteLine($"Erro: não foi possível interpretar a linha {repo.LineCount}");
-        break;
+            break;
+
+        default:
+            throw new InvalidOperationException($"Erro: não foi possível interpretar a linha {repo.LineCount}");
     }
 }
+
 
 #pragma warning disable CS8604 // Possível argumento de referência nula.
 
